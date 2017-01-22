d099b008929e20f6aed233edf51876d4a639b095
bernieyangmh <berniey@163.com>
2017-01-22 18:46:37 +0800

    tree

--- DataStructure.py
+++ DataStructure.py
@@ -411,19 +411,19 @@ class DoublyLinkedBase:
         return element                                          # help Python’s garbage collection
 
 
-class LinkedDeque(DoublyLinkedBase):
+class LinkedQeque(DoublyLinkedBase):
     """Double-ended queue implementation based on a doubly linked list."""
 
     def first(self):
         """Return the element at the front of the deque."""
         if self.is_empty():
-            raise ValueError('Deque is empty')
+            raise ValueError('Qeque is empty')
         return self.header.next._element
 
     def last(self):
         """Return the element at the front of the deque."""
         if self.is_empty():
-            raise ValueError('Deque is empty')
+            raise ValueError('Qeque is empty')
         return self._trailer._prev._element
 
     def insert_first(self, e):
@@ -437,13 +437,13 @@ class LinkedDeque(DoublyLinkedBase):
     def delete_first(self):
         """Remove and return the element from the front of the deque."""
         if self.is_empty():
-            raise ValueError('Deque is empty')
+            raise ValueError('Qeque is empty')
         return self._delete_node(self._header._next)
 
     def delete_last(self):
         """Remove and return the element from the back of the deque."""
         if self.is_empty():
-            raise ValueError('Deque is empty')
+            raise ValueError('Qeque is empty')
         return self._delete_node(self._trailer._prev)
 
 
@@ -766,9 +766,195 @@ class BinaryTree(Tree):
                 return self.right(parent)
             else:
                 return self.left(parent)
+
     def children(self, p):
         """Generate an iteration children"""
         if self.left(p) is not None:
             yield self.left(p)
         if self.right(p) is not None:
             yield self.right(p)
+
+
+class LinkedBinaryTree(BinaryTree):
+
+    class _Node:
+        __slots__ = '_element', '_parent', '_left', '_right'
+
+        def __init__(self, element, parent=None, left=None, right=None):
+            self._element = element
+            self._parent = parent
+            self.__left = left
+            self._right = right
+
+    class Position(BinaryTree.Position):
+
+        def __init__(self, container, node):
+            self._container = container
+            self._node = node
+
+        def element(self):
+            return self._node._element
+
+        def __eq__(self, other):
+            return type(other) is type(self) and other._node is self._node
+
+    def _validate(self, p):
+        if not isinstance(p, self.Position):
+            raise TypeError('p must be proper Position type')
+        if p._container is not self:
+            raise ValueError('p does not belong to this container')
+        if p._node._parent is p._node:
+            raise ValueError('p is no longer valid')
+        return p._node
+
+    def _make_position(self, node):
+        return self.Position(self, node) if node is not None else None
+
+    def __init__(self):
+        self._root = None
+        self.__size = 0
+
+    def __len__(self):                                                      # O(1)
+        return self._size
+
+    # O(1) Methods
+    def root(self):
+        return self._make_position(self._root)
+
+    def parent(self, p):
+        node = self._validate(p)
+        return self._make_position(node._parent)
+
+    def left(self, p):
+        node = self._validate(p)
+        return self._make_position(node._left)
+
+    def right(self, p):
+        node = self._validate(p)
+        return self._make_position(node._right)
+
+    def num_children(self, p):
+        node = self._validate(p)
+        count = 0
+        if node._left is not None:
+            count += 1
+        if node._right is not None:
+            count += 1
+        return count
+
+    def _add_root(self, e):
+        if self._root is not None:
+            raise ValueError('Root exists')
+        self._size = 1
+        self._root = self._Node(e)
+        return self._make_position(self._root)
+
+    def _add_left(self, p, e):
+        node = self._validate(p)
+        if node._left is not None:
+            raise ValueError('Left child exists')
+        self._size += 1
+        node._left = self._Node(e, node)
+        return self._make_position(node._left)
+
+    def _add_right(self, p, e):
+        node = self._validate(p)
+        if node._right is not None:
+            raise ValueError('Right child exists')
+        self._size += 1
+        node._right = self._Node(e, node)
+        return self._make_position(node._right)
+
+    def _replace(self, p, e):
+        node =self._validate(p)
+        old =node._element
+        node._element = e
+        return old
+
+    def _delete(self, p):
+        node = self._validate(p)
+        if self.num_children(p) == 2:
+            raise ValueError('p has two children')
+        child = node._left if node._left else node._right
+        if child is not None:
+            child._parent = node._parent
+        if node is self._root:
+            self._root = child
+        else:
+            parent = node._parent
+            if node is parent._left:
+                parent._left = child
+            else:
+                parent._right = child
+        self._size -= 1
+        node._parent = node
+        return node._element
+
+    def _attch(self, p, t1, t2):
+        node = self._validate(p)
+        if not self.is_leaf(p):
+            raise ValueError('position must be leaf')
+        if not type(self) is type(t1) is type(t2):
+            raise TypeError('tree types must match')
+        self._size += len(t1) + len(t2)
+        if not t1.is_empty():
+            t1._root._parent = node
+            node._left = t1._root
+            t1._root = None
+            t1._size = 0
+        if not t2.is_empty():
+            t2._root._parent = node
+            node._right = t2._root
+            t2._root = None
+            t2._size = 0
+
+    def perorder(self):
+        """前序遍历"""
+        if not self.is_empty():
+            for p in self._subtree_preorder(self.root()):
+                yield p
+
+    def _subtree_preorder(self, p):
+        yield p
+        for c in self.children(p):
+            for other in self._subtree_preorder(c):
+                yield other
+
+
+    def postorder(self):
+        """后序遍历"""
+        if not self.is_empty():
+            for p in self._subtree_postorder(self.root()):
+                yield p
+
+    def _subtree_postorder(self, p):
+        for c in self.children(p):
+            for other in self._subtree_postorder(c):
+                yield other
+        yield p
+
+    def inorder(self):
+        """中序遍历"""
+        if not self.is_empty():
+            for p in self._subtree_inorder(self.root()):
+                yield p
+
+    def _subtree_inorder(self, p):
+        if self.left(p) is not None:
+            for other in self._subtree_inorder(self.left(p)):
+                yield other
+        yield p
+        if self.right(p) is not None:
+            for other in self._subtree_inorder(self.right(p)):
+                yield other
+
+    def breadthfirst(self):
+        """广度遍历"""
+        if not self.is_empty():
+            fringe = LinkedQeque()
+            fringe.enqueue(self.root())
+            while not fringe.is_empty():
+                p = fringe.dequeue()
+                yield p
+                for c in self.children(p):
+                    fringe.enqueue(c)
