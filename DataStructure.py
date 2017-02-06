c95cbd512bfaff6748c149a69f506d0abac53643
bernieyangmh <berniey@163.com>
2017-02-06 13:34:46 +0800

    hpq

--- DataStructure.py
+++ DataStructure.py
@@ -1213,3 +1213,70 @@ class SortedPriorityQueue(PriorityQueueBase):
             raise ValueError('Priority queue is empty')
         item = self._data.delete(self._data.first())
         return (item._key, item._value)
+
+
+class HeapPriorityQueue(PriorityQueueBase):
+    """A min-oriented priority queue implemented with a binary heap.
+        add & remove_min both are O(logN)
+    """
+
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def _parent(self, j):
+        return (j-1)//2
+
+    def _left(self, j):
+        return 2*j+1
+
+    def _right(self, j):
+        return 2*j+2
+
+    def _has_left(self, j):
+        return self._left(j) < len(self._data)
+
+    def _has_right(self, j):
+        return self._left(j) < len(self._data)
+
+    def _swap(self, i, j):
+        self._data[i], self._data[j] = self._data[j], self._data[i]
+
+    def _uphead(self, j):
+        parent = self._parent(j)
+        if j > 0 and self._data[j] < self._data[parent]:
+            self._swap(j, parent)
+            self._uphead(parent)
+    def _downheap(self, j):
+        if self._has_left(j):
+            left = self._left(j)
+            small_child = left
+            if self._has_right(j):
+                right = self._right(j)
+                if self._data[right] < self._data[left]:
+                    small_child = right
+            if self._data[small_child] < self._data[j]:
+                self._swap(j, small_child)
+                self._downheap(small_child)
+
+    def add(self, key, value):
+        self._data.append(self._item(key, value))
+        self._uphead(len(self._data) - 1)
+
+    def min(self):
+        if self.is_empty():
+            raise ValueError('Priority queue is empty')
+        item = self._data[0]
+        return (item._key, item._value)
+
+
+    def remove_min(self):
+        if self.is_empty():
+            raise ValueError('Priority queue is empty')
+        self._swap(0, len(self._data) - 1)
+        item = self._data.pop()
+        self._downheap(0)
+        return (item._key, item._value)
+
